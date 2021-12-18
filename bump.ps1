09fad0a4fc64b44dc585ac5ee3ce0f23c422cbf9
Thaison Nguyen <thieson08@me.com>
2021-12-18 09:03:53 +0100

    Fix bump script

--- bump.ps1
+++ bump.ps1
@@ -10,11 +10,11 @@ $tag = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).tag_name
 $appinfo = Get-IniContent ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
 $appinfo["Version"]["PackageVersion"]=-join($tag,".0")
 $appinfo["Version"]["DisplayVersion"]=$tag
-$appinfo | Out-IniFile -Force -FilePath ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
+$appinfo | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
 
 $installer = Get-IniContent ".\VSCodiumPortable\App\AppInfo\installer.ini"
 $installer["DownloadFiles"]["DownloadURL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-ia32-",$tag,".zip")
 $installer["DownloadFiles"]["DownloadFilename"]=-join("VSCodium-win32-ia32-",$tag,".zip")
 $installer["DownloadFiles"]["Download2URL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-x64-",$tag,".zip")
 $installer["DownloadFiles"]["Download2Filename"]=-join("VSCodium-win32-x64-",$tag,".zip")
-$installer | Out-IniFile -Force -FilePath ".\VSCodiumPortable\App\AppInfo\installer.ini"
+$installer | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\installer.ini"
