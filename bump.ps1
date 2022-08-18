a9d00326206269e32efc22d8748350a0d0cd6950
thosoo <thieson08@me.com>
2022-08-18 22:34:58 +0200

    Update bump.ps1
    Only update if there are new releases upstream

--- bump.ps1
+++ bump.ps1
@@ -7,14 +7,18 @@ try {
 $repoName = "VSCodium/vscodium"
 $releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
 $tag = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).tag_name
-$appinfo = Get-IniContent ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
-$appinfo["Version"]["PackageVersion"]=-join($tag,".0")
-$appinfo["Version"]["DisplayVersion"]=$tag
-$appinfo | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
 
-$installer = Get-IniContent ".\VSCodiumPortable\App\AppInfo\installer.ini"
-$installer["DownloadFiles"]["DownloadURL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-ia32-",$tag,".zip")
-$installer["DownloadFiles"]["DownloadFilename"]=-join("VSCodium-win32-ia32-",$tag,".zip")
-$installer["DownloadFiles"]["Download2URL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-x64-",$tag,".zip")
-$installer["DownloadFiles"]["Download2Filename"]=-join("VSCodium-win32-x64-",$tag,".zip")
-$installer | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\installer.ini"
+$appinfo = Get-IniContent ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
+if ($appinfo["Version"]["DisplayVersion"] -ne $tag)
+{
+  $appinfo["Version"]["PackageVersion"]=-join($tag,".0")
+  $appinfo["Version"]["DisplayVersion"]=$tag
+  $appinfo | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
+
+  $installer = Get-IniContent ".\VSCodiumPortable\App\AppInfo\installer.ini"
+  $installer["DownloadFiles"]["DownloadURL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-ia32-",$tag,".zip")
+  $installer["DownloadFiles"]["DownloadFilename"]=-join("VSCodium-win32-ia32-",$tag,".zip")
+  $installer["DownloadFiles"]["Download2URL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-x64-",$tag,".zip")
+  $installer["DownloadFiles"]["Download2Filename"]=-join("VSCodium-win32-x64-",$tag,".zip")
+  $installer | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\installer.ini"
+}
