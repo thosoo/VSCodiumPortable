890138119f5531b9e304fb0878b8564fd997e49f
thosoo <thieson08@me.com>
2022-08-19 01:04:31 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 dir
 Write-Host "Starting Launcher Generator"
-Start-Process "D:\a\VSCodiumPortable\VSCodiumPortable\PortableApps.comLauncher\PortableApps.comLauncherGenerator.exe" -ArgumentList "D:\a\VSCodiumPortable\VSCodiumPortable\VSCodiumPortable\" -NoNewWindow -Wait
+Start-Process "D:\a\VSCodiumPortable\VSCodiumPortable\PortableApps.comLauncher\PortableApps.comLauncherGenerator.exe" -ArgumentList "D:\a\VSCodiumPortable\VSCodiumPortable\VSCodiumPortable" -NoNewWindow -Wait
 Write-Host "Starting Installer Generator"
-Start-Process "D:\a\VSCodiumPortable\VSCodiumPortable\PortableApps.comInstaller\PortableApps.comInstaller.exe" -ArgumentList "D:\a\VSCodiumPortable\VSCodiumPortable\VSCodiumPortable\" -NoNewWindow -Wait
+Start-Process "D:\a\VSCodiumPortable\VSCodiumPortable\PortableApps.comInstaller\PortableApps.comInstaller.exe" -ArgumentList "D:\a\VSCodiumPortable\VSCodiumPortable\VSCodiumPortable" -NoNewWindow -Wait
 dir
