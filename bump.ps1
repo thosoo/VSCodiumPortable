5a8dbc112d467cb7ca213220686224155c19aea4
thosoo <thieson08@me.com>
2022-08-18 22:49:04 +0200

    Add GITHUB_env to bumpscript

--- bump.ps1
+++ bump.ps1
@@ -21,4 +21,10 @@ if ($appinfo["Version"]["DisplayVersion"] -ne $tag)
   $installer["DownloadFiles"]["Download2URL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-x64-",$tag,".zip")
   $installer["DownloadFiles"]["Download2Filename"]=-join("VSCodium-win32-x64-",$tag,".zip")
   $installer | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\installer.ini"
+  Write-Host "Bumped to "+$tag
+  echo "SHOULD_COMMIT=yes" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+}
+else{
+  Write-Host "No changes."
+  echo "SHOULD_COMMIT=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 }
