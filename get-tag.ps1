dcca3fb05d36727317ea244066b07f1ea83fae12
thosoo <mail@thaison-nguyen.de>
2022-08-19 14:34:23 +0200

    Create get-tag.ps1

---
+++ get-tag.ps1
@@ -0,0 +1,4 @@
+$repoName = "thosoo/VSCodiumPortable"
+$releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
+$tag = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).tag_name
+echo $tag
