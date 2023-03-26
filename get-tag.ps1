2f6d6f81168414d1c1b87b945e988770016dc492
thosoo <mail@thaison-nguyen.de>
2023-03-26 10:35:13 +0200

    Use Invoke-RestMethod

--- get-tag.ps1
+++ get-tag.ps1
@@ -1,4 +1,4 @@
 $repoName = "thosoo/VSCodiumPortable"
 $releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
-$tag = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).tag_name
+$tag = (Invoke-RestMethod -Uri $releasesUri).tag_name
 echo $tag
