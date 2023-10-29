0dbfcd2c5e73e654d917cf2478ea06908fbd88fe
thosoo <mail@thaison-nguyen.de>
2023-10-29 17:38:11 +0100

    Update bump.ps1
    now really revert

--- bump.ps1
+++ bump.ps1
@@ -1,13 +1,9 @@
-# Import PsIni module, install it if not found
-$module = Get-Module -Name PsIni -ErrorAction SilentlyContinue
-if (!$module) {
-    Install-Module -Scope CurrentUser PsIni
-    Import-Module PsIni
-} else {
-    Import-Module PsIni
+try {
+  Import-Module PsIni
+} catch {
+  Install-Module -Scope CurrentUser PsIni
+  Import-Module PsIni
 }
-
-# Define repository name and API endpoint
 $repoName = "VSCodium/vscodium"
 $releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
 try { $tag = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).tag_name }
@@ -19,36 +15,23 @@ catch {
 
 echo "UPSTREAM_TAG=$tag" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 
-$appinfoFilePath = ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
-if ((Test-Path $appinfoFilePath) -and (Get-IniContent $appinfoFilePath).Version.DisplayVersion -ne $tag) {
-    $appinfo = @{
-        Version = @{
-            DisplayVersion = $tag
-            PackageVersion = "$tag.0"
-        }
-    }
-    Out-IniFile @appinfo -Force -Encoding ASCII -Pretty -FilePath $appinfoFilePath
+$appinfo = Get-IniContent ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
+if ($appinfo["Version"]["DisplayVersion"] -ne $tag)
+{
+  $appinfo["Version"]["PackageVersion"]=-join($tag,".0")
+  $appinfo["Version"]["DisplayVersion"]=$tag
+  $appinfo | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
 
-    $installerFilePath = ".\VSCodiumPortable\App\AppInfo\installer.ini"
-    if (Test-Path $installerFilePath) {
-        $downloadURL = "https://github.com/VSCodium/vscodium/releases/download/$tag/VSCodium-win32-ia32-$tag.zip"
-        $downloadFilename = "VSCodium-win32-ia32-$tag.zip"
-        $download2URL = "https://github.com/VSCodium/vscodium/releases/download/$tag/VSCodium-win32-x64-$tag.zip"
-        $download2Filename = "VSCodium-win32-x64-$tag.zip"
-        $installer = @{
-            DownloadFiles = @{
-                DownloadURL = $downloadURL
-                DownloadFilename = $downloadFilename
-                Download2URL = $download2URL
-                Download2Filename = $download2Filename
-            }
-        }
-        Out-IniFile @installer -Force -Encoding ASCII -Pretty -FilePath $installerFilePath
-        Write-Host "Bumped to $tag"
-        Write-Output "SHOULD_COMMIT=yes" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
-    }
+  $installer = Get-IniContent ".\VSCodiumPortable\App\AppInfo\installer.ini"
+  $installer["DownloadFiles"]["DownloadURL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-ia32-",$tag,".zip")
+  $installer["DownloadFiles"]["DownloadFilename"]=-join("VSCodium-win32-ia32-",$tag,".zip")
+  $installer["DownloadFiles"]["Download2URL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-x64-",$tag,".zip")
+  $installer["DownloadFiles"]["Download2Filename"]=-join("VSCodium-win32-x64-",$tag,".zip")
+  $installer | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\installer.ini"
+  Write-Host "Bumped to "+$tag
+  echo "SHOULD_COMMIT=yes" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 }
-else {
-    Write-Host "No changes."
-    Write-Output "SHOULD_COMMIT=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+else{
+  Write-Host "No changes."
+  echo "SHOULD_COMMIT=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 }
