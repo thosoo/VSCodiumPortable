6bb3a5f1a7205185959b59e3d6f466821e1a0970
thosoo <mail@thaison-nguyen.de>
2024-04-22 10:20:23 +0200

    remove 32 bits as upstream removed 32 bits

--- bump.ps1
+++ bump.ps1
@@ -23,10 +23,8 @@ if ($appinfo["Version"]["DisplayVersion"] -ne $tag)
   $appinfo | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\appinfo.ini"
 
   $installer = Get-IniContent ".\VSCodiumPortable\App\AppInfo\installer.ini"
-  $installer["DownloadFiles"]["DownloadURL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-ia32-",$tag,".zip")
-  $installer["DownloadFiles"]["DownloadFilename"]=-join("VSCodium-win32-ia32-",$tag,".zip")
-  $installer["DownloadFiles"]["Download2URL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-x64-",$tag,".zip")
-  $installer["DownloadFiles"]["Download2Filename"]=-join("VSCodium-win32-x64-",$tag,".zip")
+  $installer["DownloadFiles"]["DownloadURL"]=-join("https://github.com/VSCodium/vscodium/releases/download/",$tag,"/VSCodium-win32-x64-",$tag,".zip")
+  $installer["DownloadFiles"]["DownloadFilename"]=-join("VSCodium-win32-x64-",$tag,".zip")
   $installer | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\VSCodiumPortable\App\AppInfo\installer.ini"
   Write-Host "Bumped to "+$tag
   echo "SHOULD_COMMIT=yes" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
